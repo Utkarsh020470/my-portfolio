78f9c6a26560082a4df17ab03a108243cb7efa86
Utkarsh Saxena <utkarsh2807saxena@gmail.com>
2025-04-25 04:33:09 +0530

    fix: update configuration for proper JSX handling

--- vite.config.js
+++ vite.config.js
@@ -18,9 +18,9 @@ export default defineConfig({
     rollupOptions: {
       output: {
         format: 'es',
-        entryFileNames: 'assets/[name].[hash].js',
-        chunkFileNames: 'assets/[name].[hash].js',
-        assetFileNames: 'assets/[name].[hash].[ext]',
+        entryFileNames: 'assets/[name].js',
+        chunkFileNames: 'assets/[name].js',
+        assetFileNames: 'assets/[name].[ext]',
       },
     },
   },
