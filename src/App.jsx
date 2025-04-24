5ab9dfe2c3a94a01f7afb1124bdee5881f1841c5
Utkarsh Saxena <utkarsh2807saxena@gmail.com>
2025-04-25 04:16:54 +0530

    fix: update routing and paths for GitHub Pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import { ThemeProvider } from './components/ThemeProvider';
 import './App.css';
 import Navbar from './components/navbar';
