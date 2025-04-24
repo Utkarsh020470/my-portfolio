8206fe3db4f051bf2f56cafd817bf374ae6706e6
Utkarsh Saxena <utkarsh2807saxena@gmail.com>
2025-04-25 04:14:11 +0530

    fix: update base path for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import path from 'path';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/portfolio-vite-v2/',
+  base: '/my-portfolio/',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
